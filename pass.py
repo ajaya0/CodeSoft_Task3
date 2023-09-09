ac8f4c660c96dd4e8c8fe1e12833bea68963b013
Ajaya Kumar Jena <109841864+ajaya0@users.noreply.github.com>
2023-09-09 17:07:26 +0530

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = int(input("Enter Your Password Length: "))
+    generated_password = generate_password(password_length)
+    print("Your Password :", generated_password)
